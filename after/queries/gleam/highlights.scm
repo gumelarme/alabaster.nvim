55ddcc240ce79a206d8908fb072403e95788b6fa
gumelarme <gumelar.pn@gmail.com>
2026-02-27 03:26:01 +0800

    fix: remove var/const declarration, add anonymous func, hole, escape sequence

--- after/queries/gleam/highlights.scm
+++ after/queries/gleam/highlights.scm
@@ -13,23 +13,13 @@
   name: (constructor_name) @AlabasterDefinition) 
 
 (function name: (identifier) @AlabasterDefinition) 
-(constant name: (identifier) @AlabasterConstant) 
+(let pattern: (identifier) @AlabasterDefinition
+     value: (anonymous_function))
 
-;; Variable declarations 
-(let pattern: 
-  [(identifier) @AlabasterDefinition
-   (_ (identifier) @AlabasterDefinition)
-   (_ (_ (identifier) @AlabasterDefinition))])
-
-(use_assignment 
-  [(identifier) @AlabasterDefinition
-   (_ (identifier) @AlabasterDefinition)
-   (_ (_ (identifier) @AlabasterDefinition))])
-
-(record_pattern_argument [(label)] @AlabasterDefinition)
+(constant name: (identifier) @AlabasterDefinition) 
 
 ;; Disable highlight on _*
 (discard) @AlabasterBase
+(hole) @AlabasterBase
 
-;; Echo special keyword 
-(echo) @Comment
+(escape_sequence) @AlabasterConstant
